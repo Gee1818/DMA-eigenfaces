3b947c664a14f87a67099db408f048a7323564af
Gee1818 <g1sw@hotmail.com>
2024-03-12 23:16:21 -0300

    Add loop through the files

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ A script to extract the faces from images inside a folder
 
 The folder where the images are must be modified to your path in the variable `path`
 
+The folder where the images will be saved must be modified to your path in the variable `output_path`
+
 Necessary libraries cv2
 `pip install opencv-python`
--- face_extraction.py
+++ face_extraction.py
@@ -7,8 +7,9 @@ import cv2
 path = "/home/ge/MCD/Data Mining Avanzado/DMA-eigenfaces-photos/"
 output_path = "/home/ge/MCD/Data Mining Avanzado/DMA-eigenfaces-output/"
 # Commented for thios to work in REPL
-# script_path = os.path.dirname(__file__)
-script_path = "/home/ge/MCD/Data Mining Avanzado/DMA-eigenfaces/"
+script_path = os.path.dirname(__file__) + "/"
+print(script_path)
+# script_path = "/home/ge/MCD/Data Mining Avanzado/DMA-eigenfaces/"
 
 # Read names
 names = []
@@ -21,7 +22,7 @@ casc_path = script_path + "haarcascade_frontalface_alt2.xml"
 face_cascade = cv2.CascadeClassifier(casc_path)
 
 
-def crop_img(raw_img, face_classifier):
+def crop_img(raw_img, face_classifier, name, number, output_path):
     img = cv2.imread(raw_img)
     # Convert to grey
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -31,14 +32,21 @@ def crop_img(raw_img, face_classifier):
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
         face = img[y : y + h, x : x + w]
         # cv2.imshow("face", face)
-        cv2.imwrite("face.jpg", face)
+        number = str(number)
+        cv2.imwrite(output_path + number + name + ".jpg", face)
     # cv2.imshow("img", img)
     cv2.waitKey(0)
 
 
-test = "/home/ge/MCD/Data Mining Avanzado/DMA-eigenfaces-photos/Lautaro/20240309_112220.jpg"
-print(test)
-crop_img(test, face_cascade)
+# TODO: Convert images in .HEIC format
 
-
-# TODO: Set a loop for all the folders and all the files. Write as number-person.jpg
+for person in names:
+    current_dir = path + person + "/"
+    print("Current person is: " + person)
+    i = 0
+    for file in os.listdir(current_dir):
+        if file[-3:] == "jpg":
+            i += 1
+            current_img = current_dir + file
+            print("Current file is: " + file)
+            crop_img(current_img, face_cascade, person, i, output_path)
