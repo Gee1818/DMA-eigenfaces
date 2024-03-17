345d0481655715ab8435e43afb4326638da25289
Gee1818 <g1sw@hotmail.com>
2024-03-17 14:43:04 -0300

    Various tweaks to output files for eigenfaces

--- face_extraction.py
+++ face_extraction.py
@@ -1,6 +1,8 @@
 import os
 
 import cv2
+import pyheif
+from PIL import Image
 
 # Set path to photos
 
@@ -18,7 +20,7 @@ for dir in os.listdir(path):
 names.count
 
 # Load the cascade
-casc_path = script_path + "haarcascade_frontalface_alt2.xml"
+casc_path = script_path + "haarcascade_frontalface_alt.xml"
 face_cascade = cv2.CascadeClassifier(casc_path)
 
 
@@ -27,10 +29,11 @@ def crop_img(raw_img, face_classifier, name, number, output_path):
     # Convert to grey
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Detect the face
-    face = face_classifier.detectMultiScale(gray_img, 1.1, 4)
+    face = face_classifier.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=6)
     for x, y, w, h in face:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        face = img[y : y + h, x : x + w]
+        # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        face = gray_img[y : y + h, x : x + w]
+        face = cv2.resize(face, (30, 30))
         # cv2.imshow("face", face)
         number = str(number)
         cv2.imwrite(output_path + name + number + ".jpg", face)
@@ -40,12 +43,13 @@ def crop_img(raw_img, face_classifier, name, number, output_path):
 
 # TODO: Convert images in .HEIC format
 
+
 for person in names:
     current_dir = path + person + "/"
     print("Current person is: " + person)
     i = 0
     for file in os.listdir(current_dir):
-        if file[-3:] == "jpg":
+        if file[-4:] != "HEIC":
             i += 1
             current_img = current_dir + file
             print("Current file is: " + file)
