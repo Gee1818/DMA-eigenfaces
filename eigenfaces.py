b44219858ce5f35e27eb37dd4ae8782bc226a99e
Gee1818 <g1sw@hotmail.com>
2024-04-16 22:02:44 -0300

    .

--- eigenfaces.py
+++ eigenfaces.py
@@ -9,24 +9,28 @@ from numpy.lib.type_check import real
 from functions import *
 
 # Define path for input images
-path = "/home/frank/maestria_mcd/nuestras_caras/Nuestras Caras-20240321T193416Z-001/test_photos/"
-    
-    
-     
+# path = "/home/frank/maestria_mcd/nuestras_caras/Nuestras Caras-20240321T193416Z-001/test_photos/"
+path = "/home/ge/MCD/Data Mining Avanzado/DMA-eigenfaces-output/"
+
+
 def main():
-    
+
     # Read images
-    images, names = read_images(path)  # Images matrix -->(num_images, 900), names -->list
+    images, names = read_images(
+        path
+    )  # Images matrix -->(num_images, 900), names -->list
 
     # Select training set
-    train, test, train_idx, test_idx, train_names, test_names = select_training_set(images, names, 8)  # train --> matrix(num_images*names, 900)
+    train, test, train_idx, test_idx, train_names, test_names = select_training_set(
+        images, names, 8
+    )  # train --> matrix(num_images*names, 900)
 
     # Calculate mean face
     mean_face = calculate_mean_face(train)  # array(900,)
 
     # Plot mean face
-    #plt.imshow(mean_face.reshape(30, 30), cmap="gray")
-    #plt.show()
+    # plt.imshow(mean_face.reshape(30, 30), cmap="gray")
+    # plt.show()
 
     # Subtract mean face from images
     train_standarized = substract_mean_face(
@@ -42,10 +46,10 @@ def main():
     )  # array(900,), matrix(900, 900)
 
     # Plot eigenfaces
-    #plot_images(np.real(eigenvectors.T), range(1, 21), 4, 5, 0, 20)
+    # plot_images(np.real(eigenvectors.T), range(1, 21), 4, 5, 0, 20)
 
     # Calculate explained variance
-    #explained_variance = calculate_explained_variance(eigenvalues)  # array(900,)
+    # explained_variance = calculate_explained_variance(eigenvalues)  # array(900,)
     n_components = 60
     ##print(explained_variance[:n_components])
     reduced_eigenvectors = eigenvectors[:, :n_components]  # matrix(900, n_components)
@@ -55,7 +59,6 @@ def main():
         reduced_eigenvectors, train_standarized
     )  # matrix(num_images*names, n_components)
 
-
     # Add a function to find the closest face to every face in the test set
 
     print("\n")
@@ -63,70 +66,73 @@ def main():
     print(f"Matrix dimensions: {reduced_eigenface_space.shape}")
     print(f"Comparing {reduced_eigenface_space.shape[0]} vectors with each other..")
 
-    
     # Distances between vectors
-    distances = np.array(reduced_eigenface_space.shape[0] * [reduced_eigenface_space.shape[0] * [float('nan')]])
-    
+    distances = np.array(
+        reduced_eigenface_space.shape[0]
+        * [reduced_eigenface_space.shape[0] * [float("nan")]]
+    )
+
     for i in range(reduced_eigenface_space.shape[0]):
         for j in range(reduced_eigenface_space.shape[0]):
-            
-            if i // 8 == j // 8: # if vectors belong to the same person...
-                continue # ...skip
-            
+
+            if i // 8 == j // 8:  # if vectors belong to the same person...
+                continue  # ...skip
+
             # Calculate distance between ith and jth vector
-            dist = np.linalg.norm(reduced_eigenface_space[i] - reduced_eigenface_space[j])
-            
+            dist = np.linalg.norm(
+                reduced_eigenface_space[i] - reduced_eigenface_space[j]
+            )
+
             # Add to matrix
             distances[i][j] = dist
             distances[j][i] = dist
-            
+
     index_max_dist = np.unravel_index(np.nanargmax(distances), distances.shape)
-                    
-    
+
     # Closest faces
     index_min_dist = np.unravel_index(np.nanargmin(distances), distances.shape)
     comb_min_dis = [train_names[index_min_dist[0]], train_names[index_min_dist[1]]]
     print("Min distance:", distances[index_min_dist])
     print("Closest faces are:", comb_min_dis)
-    
+
     # Most distant faces
     index_max_dist = np.unravel_index(np.nanargmax(distances), distances.shape)
     comb_max_dis = [train_names[index_max_dist[0]], train_names[index_max_dist[1]]]
     print("Max distance:", distances[index_max_dist])
     print("Most distant faces are:", comb_max_dis)
-    
+
     # Print the faces
     # Get the faces matrix
-    distant_faces = np.column_stack((images[train_idx[index_max_dist[0]]], images[train_idx[index_max_dist[1]]]))
+    distant_faces = np.column_stack(
+        (images[train_idx[index_max_dist[0]]], images[train_idx[index_max_dist[1]]])
+    )
     # Get the names:
     names = [train_names[index_max_dist[0]], train_names[index_max_dist[1]]]
     # Plot:
-    plot_images(np.real(distant_faces.T), [names[0] , names[1]], 1, 2, 0, 2)
-    
-    
+    plot_images(np.real(distant_faces.T), [names[0], names[1]], 1, 2, 0, 2)
+
     # Create two DataFrames (train + test) with the first 60 PCs of each image
-    for (tag, split, names) in [("train", train, train_names), ("test", test, test_names)]:
+    for tag, split, names in [
+        ("train", train, train_names),
+        ("test", test, test_names),
+    ]:
         centered_imgs = substract_mean_face(split, mean_face)
         eigenspace = create_eigenface_space(reduced_eigenvectors, centered_imgs)
         pca_df = pd.DataFrame(eigenspace)
-        pca_df.columns = ["PC"+str(i) for i in range(1, n_components+1)]
+        pca_df.columns = ["PC" + str(i) for i in range(1, n_components + 1)]
         pca_df.insert(0, "Name", names, True)
-        pca_df.to_csv(f"components_{tag}.csv", index=False)
-    
+        pca_df.to_csv(f"test2-components_{tag}.csv", index=False)
 
-
-    # Testing PCA accuracy. 
+    # Testing PCA accuracy.
     # Result should be a vector with boolean values which results
     # from comparing the predicted name with the real name
 
-    
-    '''
+    """
     evaluate_test(
         test, test_names, reduced_eigenvectors, train_names, reduced_eigenface_space
     )
 
-    '''
-
+    """
 
     # Project new face into eigenvectors space
     # i = 5
@@ -157,7 +163,6 @@ def main():
 
     # Plot comparison images
 
-
     # Plot images
     # plot_images(train, train_names, 2, 4, 24, 32)
     # plot_images(images, names, 5, 5, 30, 55)
@@ -165,5 +170,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
-    
+
